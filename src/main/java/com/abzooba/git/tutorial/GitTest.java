8b6f5912314a6c6f8d325ff149a9335f5626175d
Krishnaarunangsu <arunangsutech@gmail.com>
2019-03-29 08:46:34 +0530

    first commit

---
+++ src/main/java/com/abzooba/git/tutorial/GitTest.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.abzooba.git.tutorial;
+
+/**
+ * @author arunangsu.sahu
+ *
+ */
+public class GitTest {
+
+	/**
+	 * 
+	 */
+	public GitTest() {
+		// No argument Constructor
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Practice Practice Practice");
+
+	}
+
+}
